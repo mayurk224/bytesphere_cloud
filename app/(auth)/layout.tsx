409d412c1c83135ea1dbb1abde9b140264bbb275
mayurk224 <mayurkamble0250@gmail.com>
2025-04-15 09:57:43 +0530

    created auth layout and added some assets

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex min-h-screen">
+      <section className="bg-brand p-10 hidden w-1/2 items-center justify-center lg:flex xl:w-2/5">
+        <div className="flex max-h-[800px] mx-w-[430px] flex-col justify-center space-y-12">
+          <div className="flex items-center gap-2">
+            <Image src="/logo.png" alt="" width={60} height={60} />
+            <h1 className="h1 text-white">ByteSphere</h1>
+          </div>
+          <div className="space-y-5 text-white">
+            <h1 className="h2">Manage your files the best way</h1>
+            <p>this is the place you can store your all files</p>
+          </div>
+          <Image
+            src="/illustration.png"
+            alt=""
+            height={342}
+            width={342}
+            priority
+            className="transition-all hover:rotate-2 hover: scale-105"
+          />
+        </div>
+      </section>
+      <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
+        <div className="mb-16 lg:hidden">
+          <Image
+            src="/icons/logo-full-brand.svg"
+            alt="logo"
+            width={224}
+            height={82}
+            className="h-auto w-[200px] lg:w-[250px]"
+          />
+        </div>
+      {children}
+      </section>
+    </div>
+  );
+};
+
+export default Layout;
