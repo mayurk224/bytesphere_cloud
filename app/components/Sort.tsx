120d0d1862d1e03ee5738fe4749ae21daf44f700
mayurk224 <mayurkamble0250@gmail.com>
2025-04-17 22:24:12 +0530

    add Sort component and Select UI; implement sorting functionality with Shad UI Select

--- app/components/Sort.tsx
+++ app/components/Sort.tsx
@@ -1,7 +1,37 @@
+"use client";
+
 import React from "react";
 
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { usePathname, useRouter } from "next/navigation";
+import { sortTypes } from "@/constants";
+
 const Sort = () => {
-  return <div>Sort</div>;
+  const router = useRouter();
+  const path = usePathname();
+  const handleSort = (value: string) => {
+    router.push(`${path}?sort=${value}`);
+  };
+  return (
+    <Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+      <SelectTrigger className="w-[180px] sort-select">
+        <SelectValue placeholder={sortTypes[0].value} />
+      </SelectTrigger>
+      <SelectContent className="sort-select-content">
+        {sortTypes.map((type) => (
+          <SelectItem key={type.value} value={type.value}>
+            {type.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
 };
 
 export default Sort;
