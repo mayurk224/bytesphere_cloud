e64b9e88fe9eb3130bf55231d21f9362a92f9980
mayurk224 <mayurkamble0250@gmail.com>
2025-04-18 09:41:49 +0530

    Refactor type declarations in index.d.ts for improved organization and clarity

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,47 +1,78 @@
-interface UploadFileProps {
-  file: File;
-  accountId: string;
-  ownerId: string;
-  path: string;
-}
+/* eslint-disable no-unused-vars */
 
-interface SearchParamProps {
-  params?: Promise<SegmentParams>;
-  searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
-}
+declare type FileType = "document" | "image" | "video" | "audio" | "other";
 
-interface ActionType {
+declare interface ActionType {
   label: string;
   icon: string;
   value: string;
 }
 
-interface RenameFileProps {
+declare interface SearchParamProps {
+  params?: Promise<SegmentParams>;
+  searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
+}
+
+declare interface UploadFileProps {
+  file: File;
+  ownerId: string;
+  accountId: string;
+  path: string;
+}
+declare interface GetFilesProps {
+  types: FileType[];
+  searchText?: string;
+  sort?: string;
+  limit?: number;
+}
+declare interface RenameFileProps {
   fileId: string;
   name: string;
   extension: string;
   path: string;
 }
-
-interface UpdateFileUsersProps {
+declare interface UpdateFileUsersProps {
   fileId: string;
   emails: string[];
   path: string;
 }
-
-interface DeleteFileProps {
+declare interface DeleteFileProps {
   fileId: string;
   bucketFileId: string;
   path: string;
 }
 
-declare type FileType = "document" | "image" | "video" | "audio" | "other";
+declare interface FileUploaderProps {
+  ownerId: string;
+  accountId: string;
+  className?: string;
+}
 
-interface GetFilesProps {
-  types: FileType[];
-  searchText?: string;
-  sort?: string;
-  limit?: number;
+declare interface MobileNavigationProps {
+  ownerId: string;
+  accountId: string;
+  fullName: string;
+  avatar: string;
+  email: string;
+}
+declare interface SidebarProps {
+  fullName: string;
+  avatar: string;
+  email: string;
+}
+
+declare interface ThumbnailProps {
+  type: string;
+  extension: string;
+  url: string;
+  className?: string;
+  imageClassName?: string;
+}
+
+declare interface ShareInputProps {
+  file: Models.Document;
+  onInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onRemove: (email: string) => void;
 }
 
 interface TotalSpace {
